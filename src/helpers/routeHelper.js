5bf662a1a9427a99f454b3512604bfa1b3992578
Danilo Oliveira Gomes <danilo.t0841.beyond@xpi.com.br>
2018-12-11 14:49:03 -0200

    adjsuts route helper

--- src/helpers/routeHelper.js
+++ src/helpers/routeHelper.js
@@ -2,7 +2,7 @@ export const resolvePath = (path) => {
     if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
         return path;
     } else {
-        let finalUrl = `/${process.env.PUBLIC_URL}${path}`;
+        let finalUrl = `${process.env.PUBLIC_URL}${path}`;
         return finalUrl;
     }
 }
