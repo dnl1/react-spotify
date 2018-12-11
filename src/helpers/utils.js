002118d8019690bcc689448282f8de8cf49c6be6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-11 14:39:18 +0000

    Deploy dnl1/react-spotify to github.com/dnl1/react-spotify.git:gh-pages

--- src/helpers/utils.js
+++
@@ -1,9 +0,0 @@
-export const extractQueryString = search => {
-    return JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"') + '"}')
-}
-
-export const msToMinutesAndSeconds = ms => {
-    var minutes = Math.floor(ms / 60000);
-    var seconds = ((ms % 60000) / 1000).toFixed(0);
-    return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
-}
